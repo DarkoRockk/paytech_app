b9b77b228930ef94ee09304bcb10163b67b06b5e
Dmitry Shadrin <d.shadrin@fantasyinvest.club>
2024-02-27 14:48:29 +0200

    init commit

---
+++ src/main/java/com/example/paytechapp/PaytechAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.paytechapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaytechAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaytechAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/paytechapp/PaytechAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paytechapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaytechAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
